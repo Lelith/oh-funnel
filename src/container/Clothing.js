d3dce11c88e1974e8f193ec1846eae1997911487
Kathrin Holzmann <lelith@gmx.de>
2019-01-24 17:32:13 +0100

    clothing fix

--- src/container/Clothing.js
+++ src/container/Clothing.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ItemImages from './TestItems.js';
+import SVG from 'react-inlinesvg';
+
+const SectionItems = (props) => {
+  const { sectionItems, onClick } = props;
+    return (
+      <button onClick={onClick}>Item</button>
+    )
+}
 
 const Clothing = (props) => {
   const { items, onChange } = props;
-  console.table(items);
+
+  const Sections =  Object.keys(items).map((type) => {
+    return (
+      <div className="itemType" key={type}>
+        <a href="#">{type}</a>
+        <SectionItems sectionItems={items[type].items} onClick={onChange} />
+      </div>
+    );
+  });
+
   return (
     <div>
       {Sections}
