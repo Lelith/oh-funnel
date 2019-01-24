23ec9059542075ee3be40b0574da8dc36ee59bb8
Oskar Maier <oskar.maier@outfittery.de>
2019-01-24 16:44:40 +0100

    First version of interpretatory progress bars.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Wardrobe from './container/Wardrobe';
+import Interpretation from './container/Interpretation';
 import './App.css';
 
 const App = () => (
   <div className="App">
     <Wardrobe />
+    <Interpretation />
   </div>
 );
 export default App;
